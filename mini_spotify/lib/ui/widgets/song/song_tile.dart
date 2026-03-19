60a25d922a93ab1718c09c9700ec08b4d6f715a2
Samnang-Vouen <vouensamnang@gmail.com>
2026-03-19 14:07:30 +0700

    W9-01 – Fetch and display songs in the library screen

--- mini_spotify/lib/ui/widgets/song/song_tile.dart
+++ mini_spotify/lib/ui/widgets/song/song_tile.dart
@@ -25,7 +25,15 @@ class SongTile extends StatelessWidget {
         ),
         child: ListTile(
           onTap: onTap,
+          leading: ClipOval(
+            child: Image.network('${song.imageUrl}',
+            width: 45,
+            height: 45,
+            fit: BoxFit.cover,
+            ),
+          ),
           title: Text(song.title),
+          subtitle: Text('${song.duration.inMinutes} mins'),
           trailing: Text(
             isPlaying ? "Playing" : "",
             style: TextStyle(color: Colors.amber),
